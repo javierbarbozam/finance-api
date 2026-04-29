0903afb0d8e49bfc4503838907ada5951aa0a65d
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-04-29 12:38:38 -0600

    modificaciones en base de datos

--- src/main/resources/db/migration/V4__create_budget_table.sql
+++ src/main/resources/db/migration/V4__create_budget_table.sql
@@ -3,6 +3,5 @@ CREATE TABLE budget (
     amount DECIMAL(12,2) NOT NULL,
     month INT NOT NULL CHECK (month BETWEEN 1 AND 12),
     year INT NOT NULL,
-    category_id BIGINT REFERENCES category(id),
     user_id BIGINT NOT NULL REFERENCES users(id)
 );
