35bd1b5c60631ae3d1f5a423259430df05bad083
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-04-29 13:00:00 -0600

    Update Category.java

--- src/main/java/com/finance/finance_api/Model/Category.java
+++ src/main/java/com/finance/finance_api/Model/Category.java
@@ -2,12 +2,8 @@ package com.finance.finance_api.Model;
 
 import java.util.List;
 
-import com.finance.finance_api.Enum.TransactionType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
