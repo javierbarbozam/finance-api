c0d4ea3f49967fdc1d6b955f31a43ef898d17c0b
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-05-28 23:49:17 -0600

    Fix class with @component
    API testing on register and login

--- src/main/java/com/barboza/finance_api/security/JwtFilter.java
+++ src/main/java/com/barboza/finance_api/security/JwtFilter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -14,6 +15,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired JwtUtils jwtUtils;
