429842a00865c1bdac922557f3cf6f4c9e5e2fa5
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-06-10 14:44:16 -0600

    Update CategoryController.java

--- src/main/java/com/barboza/finance_api/controller/CategoryController.java
+++ src/main/java/com/barboza/finance_api/controller/CategoryController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class CategoryController {
