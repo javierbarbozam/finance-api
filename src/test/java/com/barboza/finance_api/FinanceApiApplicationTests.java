ba0852230c6c434c26872842ffc079c95f08b1e0
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-06-10 11:50:46 -0600

    Update FinanceApiApplicationTests.java

--- src/test/java/com/barboza/finance_api/FinanceApiApplicationTests.java
+++ src/test/java/com/barboza/finance_api/FinanceApiApplicationTests.java
@@ -1,6 +1,6 @@
 package com.barboza.finance_api;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
