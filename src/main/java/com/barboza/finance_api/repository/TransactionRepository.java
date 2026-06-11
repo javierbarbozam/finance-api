448ef0bebda4b320efa11af79cb5989443f6802b
Javier Barboza <106450209+javierbarbozam@users.noreply.github.com>
2026-06-11 13:06:19 -0600

    Remove @Repository

--- src/main/java/com/barboza/finance_api/repository/TransactionRepository.java
+++ src/main/java/com/barboza/finance_api/repository/TransactionRepository.java
@@ -1,15 +1,12 @@
 package com.barboza.finance_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.barboza.finance_api.entity.Transaction;
 import com.barboza.finance_api.entity.User;
 
 import java.util.List;
 
 
-@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 
     public List<Transaction> findByUserOrderByDateDesc(User user);
--- src/main/java/com/barboza/finance_api/repository/UserRepository.java
+++ src/main/java/com/barboza/finance_api/repository/UserRepository.java
@@ -3,11 +3,8 @@ package com.barboza.finance_api.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.barboza.finance_api.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
 }
